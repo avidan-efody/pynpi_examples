abbee1c14fbd4c65cb3f1c3b5839667af0fc4246
Efody <avidan.efody@intel.com>
2021-04-26 06:44:30 +0300

    Adding example of filtering non-synthesizable code from code-coverage reports using pynpi lang and cov modules

--- simple_example/top.v
+++ simple_example/top.v
@@ -3,8 +3,12 @@ module top();
     reg valid = 0;
     reg[2:0] data;
 
-    initial
-      for(int i=0;i<10;i++) 
+    reg data_is_5A;
+    reg data_is_5B;
+
+    initial begin
+      int i;
+      for(i=0;i<10;i++) 
         begin 
           #10 clk = ~clk;
 
@@ -19,5 +23,26 @@ module top();
 
           if ($time() > 100)
             $finish();
+
+          if (data == 5) begin
+            data_is_5A <= 1;
+            $display("intentional code coverage hole");
+          end
         end
+    end
+
+    always @(posedge clk)
+      begin
+        if (valid == 1) 
+          data <= 3;
+        else
+          data <= 0;
+
+        // intentional code coverage hole
+        if (data == 5) begin
+          data_is_5B <= 1;
+          $display("intentional code coverage hole");
+        end
+      end
+       
 endmodule
